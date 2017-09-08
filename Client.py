3d6b4fa49afd60a09d6883137819eca0d6f899c7
veerthiara <veerbaha@ualberta.ca>
2017-09-08 11:32:19 -0400

    Code Friday September 8
    Created a thread for the Chat GUI Window.

--- Client.py
+++ Client.py
@@ -4,6 +4,8 @@ import socket
 import time
 import threading
 from threading import Thread
+import tkinter as tk
+from tkinter import ttk
 
 
 # from SocketServer import ThreadingMixIn
@@ -13,10 +15,10 @@ class ServerThread(Thread):
         Thread.__init__(self)
         self.socket = socket
 
-        #   print "New thread started for write"
 
     def run(self):
         print("send")
+
         while True:
             #starttime = time.time()
             command = input(" Enter command: ")
@@ -51,10 +53,63 @@ threads = []
 global log
 log = 0
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.connect((TCP_IP, TCP_PORT))
 TIME_OUT = s.recv(BUFFER_SIZE)  # Server exchanges tmeout details with client at the start of every socket
 count = [1, 2, 3]
 status = 0
+class clientchatwindows():
+
+    def code(self):
+        print("runfunction")
+        self.clientwin = tk.Tk()
+        self.clientwin.title("Chat")
+
+        # self.clientwin.mainloop()
+        # chatwindowgui = windows()
+        print("self.clientwin")
+
+        print("afterchatreeive")
+
+        chatreceive = tk.StringVar()
+        print("aftertkstringintvar")
+        ttk.Label(self.clientwin, text="Chat").grid(column=0, row=0)
+        self.chatreceive = ttk.Entry(self.clientwin, width=20, textvariable=chatreceive)
+        self.chatreceive.grid(column=0, row=2)
+        sendchat = ttk.Button(self.clientwin, text="Send", command=self.gettext)  # button calls the sendchat function
+        sendchat.grid(column=1, row=2)
+        self.clientwin.protocol('WM_DELETE_WINDOW', self.closesecond)
+
+        # -----------------------------------------------------------------------------
+        # scroll bar code that is going to display
+
+        loginscroll = tk.Scrollbar(self.clientwin)
+        self.listbox = tk.Listbox(self.clientwin)
+        self.listbox.grid(column=0, row=1)
+        for i in range(100):
+            self.listbox.insert(tk.END, i)
+        self.listbox.config(yscrollcommand=loginscroll.set)
+        loginscroll.config(command=self.listbox.yview)
+
+        # ----------------------------------------------------------------------
+
+        sendto = ttk.Button(self.clientwin, text="-->>", command=self.get)
+        sendto.grid(column=1, row=1)
+        #   print "New thread started for write"
+        print("mainloop")
+        self.clientwin.mainloop()
+    def closesecond(self):
+        self.clientwin.destroy()
+        self.clientwin.quit()
+
+    def get(self):
+        printlistbox = self.listbox.get('active')
+        print(printlistbox)
+    def gettext(self):
+        gettextchatreceive=self.chatreceive.get()
+        print(gettextchatreceive)
+
+
 class clientcode():                                         # t
     def clientcodemain(self,usernamein,passwordin):
         #status = 0
@@ -87,10 +142,15 @@ class clientcode():                                         # t
                     newthread.daemon = True
                     newthread2 = ServerThreadread(s)
                     newthread2.daemon = True
+
                     newthread.start()
                     newthread2.start()
+
                     threads.append(newthread)
                     threads.append(newthread2)
+                    newmethod = clientchatwindows()
+                    t = Thread(target=newmethod.code())
+                    t.start()
                     while True:
                         for t in threads:
                             t.join(600)
--- Login.py
+++ Login.py
@@ -41,6 +41,10 @@ class login():
     def get(self):
         printlistbox=self.listbox.get('active')
         print(printlistbox)
+    def sendchatfun(self):
+        chatreceive=self.chatreceive.get()
+
+
 
 # function that is caled when login button clicked
     def loginaction(self):
@@ -55,35 +59,35 @@ class login():
         if getreturn:
 
             chatreceive=tk.StringVar()
-            print(":login successfull")
+            #print(":login successfull")
             # the main window is closed
             self.win.state("withdrawn")
             # -----------------------------------------------------------------------------
             #another window opening showing the functions
 
-            self.chat=tk.Toplevel(self.win)
-            ttk.Label(self.chat, text="Chat").grid(column=0, row=0)
-            chatreceive = ttk.Entry(self.chat, width=20, textvariable=chatreceive)
-            chatreceive.grid(column=0, row=2)
-            sendchat = ttk.Button(self.chat, text="Send")
-            sendchat.grid(column=1, row=2)
-            self.chat.protocol('WM_DELETE_WINDOW', self.closesecond)
+            #self.chat=tk.Toplevel(self.win)
+            #ttk.Label(self.chat, text="Chat").grid(column=0, row=0)
+        #   self.chatreceive = ttk.Entry(self.chat, width=20, textvariable=chatreceive)
+        #    self.chatreceive.grid(column=0, row=2)
+         #   sendchat = ttk.Button(self.chat, text="Send", command=self.sendchatfun)  # button calls the sendchat function
+        #    sendchat.grid(column=1, row=2)
+       #     self.chat.protocol('WM_DELETE_WINDOW', self.closesecond)
 
             # -----------------------------------------------------------------------------
             # scroll bar code that is going to display
 
-            loginscroll = tk.Scrollbar(self.chat)
-            self.listbox = tk.Listbox(self.chat)
-            self.listbox.grid(column=0,row=1)
-            for i in range(100):
-                self.listbox.insert(tk.END,i)
-            self.listbox.config(yscrollcommand=loginscroll.set)
-            loginscroll.config(command=self.listbox.yview)
+      #      loginscroll = tk.Scrollbar(self.chat)
+     #       self.listbox = tk.Listbox(self.chat)
+    #        self.listbox.grid(column=0,row=1)
+   #         for i in range(100):
+  #              self.listbox.insert(tk.END,i)
+ #           self.listbox.config(yscrollcommand=loginscroll.set)
+#            loginscroll.config(command=self.listbox.yview)
 
             # ----------------------------------------------------------------------
 
-            sendto = ttk.Button(self.chat, text="-->>", command=self.get)
-            sendto.grid(column=1, row=1)
+            #sendto = ttk.Button(self.chat, text="-->>", command=self.get)
+            #sendto.grid(column=1, row=1)
             client=Client.clientcode()
             client.clientcodemain(recvusername,recvpassword)
 
